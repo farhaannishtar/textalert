f9a12672ca97089f0481197d32ebc9dc14ec5e9b
ai-is-awesome <irock0093@gmail.com>
2024-02-21 19:11:59 +0530

    login ui update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@ import LoginWithSMS from "@/src/components/ui/SMSLogin/LoginWithSMS";
 
 export default function Login() {
   return (
-    <LoginWithSMS />
+    <div className="flex flex-col w-1/2 mx-auto my-20">
+      <LoginWithSMS />
+    </div>
   );
 }
